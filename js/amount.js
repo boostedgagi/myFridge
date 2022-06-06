19147b6d51d32fb6514c5a0875177c6b8c56ed88
jurcekk <jurcekdavid@gmail.com>
2022-06-07 00:29:15 +0200

    conflict commit

--- js/amount.js
+++ js/amount.js
@@ -3,7 +3,6 @@ const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 let amount = document.querySelector("#amount");
 
-<<<<<<< HEAD
 add.addEventListener("click", () => (amount.value = "1 Amountunit"));
 
 plus.addEventListener("click", () => {
@@ -20,23 +19,20 @@ minus.addEventListener("click", () => {
     amount.value = number.toString() + " Amountunit";
   }
 });
-=======
-add.addEventListener("click", ()=> amount.value = "1");
 
-plus.addEventListener("click", ()=> {
-    let number = parseInt(amount.value);
-    number += 1;
+add.addEventListener("click", () => (amount.value = "1"));
+
+plus.addEventListener("click", () => {
+  let number = parseInt(amount.value);
+  number += 1;
+  amount.value = number.toString();
+});
+
+minus.addEventListener("click", () => {
+  let number = parseInt(amount.value);
+  if (number <= 0) {
+  } else {
+    number -= 1;
     amount.value = number.toString();
+  }
 });
-
-minus.addEventListener("click", ()=> {
-    let number = parseInt(amount.value);
-    if(number <= 0) {
-    }
-    else {
-        number -= 1;
-        amount.value = number.toString();
-    }
-});
-
->>>>>>> 86d41b278da7822f76b6ffb5bba5ad71375f8d0c
